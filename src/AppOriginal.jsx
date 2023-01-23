84fd90347628ebae14b59496ce21c525607ad9a8
Stephan Griesel <sgriesel@gmail.com>
2023-01-23 08:09:38 +0100

    render components

--- src/AppOriginal.jsx
+++ src/AppOriginal.jsx
@@ -1,38 +1,37 @@
-import React, { useEffect, useState } from "react";
-import CardContainer from "./components/CardContainer";
-import Footer from "./components/Footer";
-import NavBar from "./components/NavBar";
+import React, { useEffect, useState } from 'react';
+import CardContainer from './components/CardContainer';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 const App = () => {
   const [notionData, setNotionData] = useState(null);
 
   useEffect(() => {
     const fetchNotionData = async () => {
-      const data = await fetch("/api/fetchNotion");
+      const data = await fetch('/api/fetchNotion');
       const allNotionData = await data.json();
       console.log("notion data in app", allNotionData);
       setNotionData(allNotionData);
-    };
+    }
     const timer = setTimeout(() => {
       fetchNotionData();
     }, 1);
 
     return () => clearTimeout(timer);
-  }, []);
+  },[])
 
-  if (!notionData) {
-    return (
-      <div className="App bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-screen"></div>
-    );
+  if (!notionData){
+    return <div className='App bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-screen'></div>
   }
+
 
   return (
     <div className="App bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-screen">
       <NavBar />
-      <CardContainer notionData={notionData} />
+      <CardContainer notionData={notionData}/>
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
