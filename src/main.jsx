568c34db646401ee9d41523e7058bf2e381930bc
Stephan Griesel <sgriesel@gmail.com>
2023-01-23 08:36:04 +0100

    app locations update

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './AppTemp'
-// import AppOriginal from './AppOriginal'
+import AppTemp from './AppTemp'
+// import AppLive from './AppOriginal'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    {/* <AppOriginal /> */}
+    <AppTemp />
+    {/* <AppLive /> */}
   </React.StrictMode>,
 )
