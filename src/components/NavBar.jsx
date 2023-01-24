e3ad078dd4541e800dd5ca4158c3203273800815
Stephan Griesel <sgriesel@gmail.com>
2023-01-24 18:27:23 +0100

    spacing update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className='flex justify-center justify-between'>
+    <div className='flex justify-center justify-between items-center h-20 pl-5 pr-5 md:pl-0 md:pr-0'>
         <div className='flex'>
             Logo
         </div>
         <div>
         <ul className='flex'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Projects</li>
-            <li>Contact</li>
+            <li className='m-2'>Home</li>
+            <li className='m-2'>About</li>
+            <li className='m-2'>Projects</li>
+            <li className='m-2'>Contact</li>
         </ul>
         </div>
     </div>
