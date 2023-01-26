e74c14ab253f28d3a18451bfcc7e006d01183996
Stephan Griesel <sgriesel@gmail.com>
2023-01-26 17:33:11 +0100

    routes set up

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AppTemp from './AppTemp'
-// import AppLive from './AppOriginal'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import Root from './routes/root';
+import ErrorPage from './error-page';
+import Contact from './routes/contact';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: 'contact',
+    element: <Contact />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppTemp />
-    {/* <AppLive /> */}
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
