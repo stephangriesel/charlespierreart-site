3c64b70219fd1618e6bebed612b316fca84bf61b
Stephan Griesel <sgriesel@gmail.com>
2023-01-26 17:34:02 +0100

    routes set up

---
+++ src/routes/root.jsx
@@ -0,0 +1,11 @@
+// import AppLive from '../AppLive';
+import AppTemp from '../AppTemp';
+
+export default function Root() {
+  return (
+    <>
+      {/* <AppLive /> */}
+      <AppTemp />
+    </>
+  );
+}
