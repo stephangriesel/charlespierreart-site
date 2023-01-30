1f4a73f5e288c22a8b1e6b032b08527badcca1cc
Stephan Griesel <sgriesel@gmail.com>
2023-01-30 08:26:59 +0100

    modal (wip)

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,46 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from './Modal';
 
 const CardContainer = ({ notionData }) => {
   const results = notionData.results;
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className='grid grid-cols-1 md:grid-cols-3'>
       {results.map((results, index) => {
         return (
-          <div key={index} className='flex flex-col p-4 m-4 rounded-lg bg-slate-200 shadow-xl transition-all duration-300 hover:shadow-none hover:scale-95 hover:opacity-100 opacity-80 cursor-pointer'>
+          <div
+            key={index}
+            className='flex flex-col p-4 m-4 rounded-lg bg-slate-200 shadow-xl transition-all duration-300 hover:shadow-none hover:scale-95 hover:opacity-100 opacity-80 cursor-pointer'
+          >
             {results.properties.Image.files.map((imageResult, index) => {
               return (
-                <img key={index} src={imageResult.external.url} />
-              )
+                <div onClick={() => setIsOpen(true)}>
+                  {isOpen && (
+                    <Modal setIsOpen={setIsOpen}>
+                      <img key={index} src={imageResult.external.url} />
+                    </Modal>
+                  )}
+                </div>
+              );
             })}
             {results.properties.Name.title.map((titleResult, index) => {
               return (
-                <h1 className='text-center font-bold' key={index}>{titleResult.plain_text}</h1>
-              )
+                <h1 className='text-center font-bold' key={index}>
+                  {titleResult.plain_text}
+                </h1>
+              );
             })}
-            {results.properties.Content.rich_text.map((contentResult, index) => {
-              return (
-                <p className='text-center' key={index}>{contentResult.plain_text}</p>
-              )
-            })}
-            {results.properties.Content.rich_text.map((contentResult, index) => {
-              return (
-                <p className='text-center' key={index}>{contentResult.plain_text}</p>
-              )
-            })}
-            <button className='bg-white p-2 mt-5 rounded text-center hover:bg-black hover:text-white' key={index}>
-              {results.properties.Btn_txt.rich_text.map((btnTxtResult, index) => {
+            {results.properties.Content.rich_text.map(
+              (contentResult, index) => {
                 return (
-                  <a key={index} href={results.properties.Link.url}>
-                    {btnTxtResult.plain_text}
-                  </a>
-                )
-              })}
+                  <p className='text-center' key={index}>
+                    {contentResult.plain_text}
+                  </p>
+                );
+              }
+            )}
+            {results.properties.Content.rich_text.map(
+              (contentResult, index) => {
+                return (
+                  <p className='text-center' key={index}>
+                    {contentResult.plain_text}
+                  </p>
+                );
+              }
+            )}
+            <button
+              className='bg-white p-2 mt-5 rounded text-center hover:bg-black hover:text-white'
+              key={index}
+            >
+              {results.properties.Btn_txt.rich_text.map(
+                (btnTxtResult, index) => {
+                  return (
+                    <a key={index} href={results.properties.Link.url}>
+                      {btnTxtResult.plain_text}
+                    </a>
+                  );
+                }
+              )}
             </button>
           </div>
-        )
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default CardContainer
+export default CardContainer;
