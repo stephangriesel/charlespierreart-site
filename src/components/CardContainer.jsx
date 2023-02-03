0c4f6498cefafa5564b05c21e9d3dfa8d158a5dd
Stephan Griesel <sgriesel@gmail.com>
2023-02-03 06:18:14 +0100

    modal add

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,14 +1,5 @@
 import React, { useState } from 'react';
-
-const Modal = ({ children, shown, close }) => {
-  return shown ? (
-    <div className='bg-black modal-backdrop' onClick={() => close()}>
-      <div className='flex justify-center modal-content' onClick={() => close()}>
-        {children}
-      </div>
-    </div>
-  ) : null;
-};
+import Modal from './Modal';
 
 const CardContainer = ({ notionData }) => {
   const results = notionData.results;
