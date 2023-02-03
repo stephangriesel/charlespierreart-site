070037c240159268b951f43057ebb89f2e4b7931
Stephan Griesel <sgriesel@gmail.com>
2023-02-03 06:36:51 +0100

    style

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Modal = ({ children, shown, close }) => {
     return shown ? (
-        <div className='bg-black modal-backdrop' onClick={() => close()}>
-            <div className='flex justify-center modal-content' onClick={() => close()}>
+        <div className='fixed top-0 left-0 bottom-0 right-0 flex justify-center items-center z-10 bg-black/40' onClick={() => close()}>
+            <div className=' w-2/3 min-h-[200px] bg-white p-6 flex justify-center' onClick={() => close()}>
                 {children}
             </div>
         </div>
