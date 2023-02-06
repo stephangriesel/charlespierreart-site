d6b90a50195befe8d6171f3d5c31b6b70f41f053
Stephan Griesel <sgriesel@gmail.com>
2023-02-06 08:24:45 +0100

    loading icon

---
+++ src/icons/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <div>
+            <svg className='animate-spin' xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#FFF" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><path d="M168,40.7a96,96,0,1,1-80,0" fill="none" stroke="#FFF" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></path></svg>
+        </div>
+    )
+}
+
+export default Loading
