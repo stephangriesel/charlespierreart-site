b7a6c20723d3a3fb07a194465f74f78811858666
Stephan Griesel <sgriesel@gmail.com>
2023-02-06 08:25:34 +0100

    background

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="flex justify-center flex-row align-middle">Footer</div>
+    <div className="flex justify-center flex-row align-middle bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">Footer</div>
   )
 }
 
