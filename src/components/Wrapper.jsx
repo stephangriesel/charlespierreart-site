cc78cee0e2e015ff1272754e10c2255d6fd09076
Stephan Griesel <sgriesel@gmail.com>
2023-02-06 08:01:18 +0100

    remove unused classes

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Wrapper = ({ children }) => {
-  return <div className='md:pl-40 md:pr-40 bg-transparent'>{children}</div>;
+  return <div>{children}</div>;
 };
 
 export default Wrapper;
