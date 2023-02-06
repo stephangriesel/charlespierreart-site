580dd27cb4d7c4c3a00209ffad9193a5885f6679
Stephan Griesel <sgriesel@gmail.com>
2023-02-06 17:58:55 +0100

    loading height position update

--- src/AppLive.jsx
+++ src/AppLive.jsx
@@ -28,7 +28,7 @@ const App = () => {
 
   if (!notionData) {
     return (
-      <div className='App flex justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-screen md:h-full'>
+      <div className='App bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-screen flex justify-center items-center flex-col'>
         <Loading />
       </div>
     );
