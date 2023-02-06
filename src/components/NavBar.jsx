fbd3d7c30836efed764fe63ca2414457822f3ef6
Stephan Griesel <sgriesel@gmail.com>
2023-02-06 08:01:00 +0100

    fix background & link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div className='flex justify-center justify-between items-center h-20 pl-5 pr-5 md:pl-0 md:pr-0'>
+    <div className='flex justify-center justify-between items-center h-20 pl-5 pr-5 md:pl-0 md:pr-0 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
       <div className='flex'>Logo</div>
       <div>
         <ul className='flex'>
@@ -11,7 +11,7 @@ const NavBar = () => {
             <Link to='/'>Home</Link>
           </li>
           <li className='m-2'>
-            <Link to='contact'>Contact</Link>
+            <Link to='/contact'>Contact</Link>
           </li>
         </ul>
       </div>
