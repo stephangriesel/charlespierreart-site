37b7146878e92c6a14b62711e3c03844a1d798d7
Stephan Griesel <sgriesel@gmail.com>
2023-02-09 08:54:49 +0100

    fix nav padding & prettier (yawn)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div className='flex justify-center justify-between items-center h-20 pl-5 pr-5 md:pl-0 md:pr-0 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
-      <div className='flex'>Logo</div>
+    <div className="flex justify-center justify-between items-center h-20 pl-5 pr-5 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <div className="flex">Logo</div>
       <div>
-        <ul className='flex'>
-          <li className='m-2'>
-            <Link to='/'>Home</Link>
+        <ul className="flex">
+          <li className="m-2">
+            <Link to="/">Home</Link>
           </li>
-          <li className='m-2'>
-            <Link to='/contact'>Contact</Link>
+          <li className="m-2">
+            <Link to="/contact">Contact</Link>
           </li>
         </ul>
       </div>
