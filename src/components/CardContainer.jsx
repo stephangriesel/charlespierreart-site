1719f2043309ef5cd8e43371fc9805747d54a7a5
Stephan Griesel <sgriesel@gmail.com>
2023-02-20 06:54:52 +0100

    filter categories

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -7,6 +7,7 @@ const CardContainer = ({ notionData }) => {
   const [selectedFilters, setSelectedFilters] = useState([]);
 
   console.log("original results", results);
+  console.log("selected filter", selectedFilters)
 
   const handleFilterClick = (filterName) => {
     if (selectedFilters.includes(filterName)) {
@@ -16,7 +17,7 @@ const CardContainer = ({ notionData }) => {
     }
   };
 
-  console.log({ handleFilterClick })
+  console.log("handle filtter click", handleFilterClick)
 
   const filters = results.reduce((acc, obj) => {
     obj.properties.Category.multi_select.forEach((filter) => {
@@ -34,80 +35,101 @@ const CardContainer = ({ notionData }) => {
     return acc;
   }, []);
 
-  console.log({ filters })
+  console.log("filters", filters)
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 cursor-pointer">
-      {results.map((results, index) => {
-        return (
-          <div
-            key={index}
-            className="flex flex-col p-4 m-4 rounded-lg bg-slate-200 shadow-xl transition-all duration-300 opacity-80 cursor-pointer"
+    <>
+      <div>
+        {filters.map((filter) => (
+          <button
+            key={filter.name}
+            onClick={() => handleFilterClick(filter.name)}
+            style={{
+              backgroundColor: selectedFilters.includes(filter.name)
+                ? filter.color
+                : 'gray',
+              color: selectedFilters.includes(filter.name)
+                ? 'white'
+                : 'black',
+              marginRight: '10px'
+            }}
           >
-            {results.properties.Image.files.map((imageResult, index) => {
-              return (
-                <div key={index}>
-                  <img
-                    src={imageResult.external.url}
-                    className="cursor-pointer"
-                    onClick={() => {
-                      toggleModal(!modalShown);
-                    }}
-                  />
-                  <Modal
-                    shown={modalShown}
-                    close={() => {
-                      toggleModal(false);
-                    }}
-                  >
-                    <img src={imageResult.external.url} />
-                  </Modal>
-                </div>
-              );
-            })}
-            {results.properties.Name.title.map((titleResult, index) => {
-              return (
-                <h1 className="text-center font-bold" key={index}>
-                  {titleResult.plain_text}
-                </h1>
-              );
-            })}
-            {results.properties.Content.rich_text.map(
-              (contentResult, index) => {
-                return (
-                  <p className="text-center" key={index}>
-                    {contentResult.plain_text}
-                  </p>
-                );
-              }
-            )}
-            {results.properties.Content.rich_text.map(
-              (contentResult, index) => {
-                return (
-                  <p className="text-center" key={index}>
-                    {contentResult.plain_text}
-                  </p>
-                );
-              }
-            )}
-            <button
-              className="bg-white p-2 mt-5 rounded text-center hover:bg-black hover:text-white"
+            {filter.name}
+          </button>
+        ))}
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 cursor-pointer">
+        {results.map((results, index) => {
+          return (
+            <div
               key={index}
+              className="flex flex-col p-4 m-4 rounded-lg bg-slate-200 shadow-xl transition-all duration-300 opacity-80 cursor-pointer"
             >
-              {results.properties.Btn_txt.rich_text.map(
-                (btnTxtResult, index) => {
+              {results.properties.Image.files.map((imageResult, index) => {
+                return (
+                  <div key={index}>
+                    <img
+                      src={imageResult.external.url}
+                      className="cursor-pointer"
+                      onClick={() => {
+                        toggleModal(!modalShown);
+                      }}
+                    />
+                    <Modal
+                      shown={modalShown}
+                      close={() => {
+                        toggleModal(false);
+                      }}
+                    >
+                      <img src={imageResult.external.url} />
+                    </Modal>
+                  </div>
+                );
+              })}
+              {results.properties.Name.title.map((titleResult, index) => {
+                return (
+                  <h1 className="text-center font-bold" key={index}>
+                    {titleResult.plain_text}
+                  </h1>
+                );
+              })}
+              {results.properties.Content.rich_text.map(
+                (contentResult, index) => {
                   return (
-                    <a key={index} href={results.properties.Link.url}>
-                      {btnTxtResult.plain_text}
-                    </a>
+                    <p className="text-center" key={index}>
+                      {contentResult.plain_text}
+                    </p>
                   );
                 }
               )}
-            </button>
-          </div>
-        );
-      })}
-    </div>
+              {results.properties.Content.rich_text.map(
+                (contentResult, index) => {
+                  return (
+                    <p className="text-center" key={index}>
+                      {contentResult.plain_text}
+                    </p>
+                  );
+                }
+              )}
+              <button
+                className="bg-white p-2 mt-5 rounded text-center hover:bg-black hover:text-white"
+                key={index}
+              >
+                {results.properties.Btn_txt.rich_text.map(
+                  (btnTxtResult, index) => {
+                    return (
+                      <a key={index} href={results.properties.Link.url}>
+                        {btnTxtResult.plain_text}
+                      </a>
+                    );
+                  }
+                )}
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
