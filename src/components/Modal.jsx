d95ef1e8f95c00c3f5530ec0e0b0080b47d320b5
Stephan Griesel <sgriesel@gmail.com>
2023-02-21 15:27:02 +0100

    change image dimensions

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Modal = ({ children, shown, close }) => {
     return shown ? (
         <div className='fixed top-0 left-0 bottom-0 right-0 flex justify-center items-center z-10 bg-black/40' onClick={() => close()}>
-            <div className=' w-2/3 min-h-[200px] bg-white p-6 flex justify-center' onClick={() => close()}>
+            <div className=' w-1/3 min-h-[120px] bg-white p-6 flex justify-center' onClick={() => close()}>
                 {children}
             </div>
         </div>
